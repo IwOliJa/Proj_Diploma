a924539e1bc0bc06e79b1256c9ee4b8e5664dcf3
OlgaKazaka <olgakazaka@yahoo.com>
2023-12-13 14:42:15 +0100

    add category tools page

--- src/components/sortingFields/index.jsx
+++ src/components/sortingFields/index.jsx
@@ -2,14 +2,29 @@ import styles from "./index.module.css";
 
 function SortingFields() {
   return (
-    <div className={styles.main_container}>
-      <p>Price</p>
-      <input type="text" value="" placeholder="from" />
-      <input type="text" value="" placeholder="to" />
-      <p>Discounted items</p>
-      <input type="text" value="" />
-      <p>Sorted</p>
-      <input type="text" placeholder="by default" />
+    <div className={styles.forms_content}>
+      <div className={styles.form_wrapper}>
+        <label className={styles.price}>Price</label>
+        <input
+          className={styles.from}
+          type="text"
+          value=""
+          placeholder="from"
+        />
+        <input className={styles.to} type="text" value="" placeholder="to" />
+      </div>
+      <div className={styles.form_wrapper}>
+        <label className={styles.discount}>Discounted items</label>
+        <input className={styles.counter} value="" />
+      </div>
+      <div className={styles.form_wrapper}>
+        <label className={styles.sorted}>Sorted</label>
+        <select className={styles.selection_field}>
+          <option value="">by default</option>
+          <option value="">fgnjfg</option>
+          <option value="">fgnjfg</option>
+        </select>
+      </div>
     </div>
   );
 }
