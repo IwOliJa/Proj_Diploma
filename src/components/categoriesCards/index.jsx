38d1511301af9cac1edf3762868483994671e02d
OlgaKazaka <olgakazaka@yahoo.com>
2023-12-21 00:20:44 +0100

    add styles correction

--- src/components/categoriesCards/index.jsx
+++ src/components/categoriesCards/index.jsx
@@ -2,13 +2,17 @@ import styles from "./index.module.css";
 
 function CategoriesCards({ image, title }) {
   return (
-    <div className={styles.categories_wrapper}>
-      <div className={styles.card_wrapper}>
-        <button className={styles.image}>
-          <img src={"http://localhost:3333" + image} alt={title} />
-        </button>
-        <p className={styles.card_name}>{title}</p>
-      </div>
+    <div className={styles.cards_wrapper}>
+      {/* <div className={styles.card_wrapper}> */}
+      <button className={styles.card_box}>
+        <img
+          className={styles.image}
+          src={"http://localhost:3333" + image}
+          alt={title}
+        />
+      </button>
+      <p className={styles.card_name}>{title}</p>
+      {/* </div> */}
     </div>
   );
 }
