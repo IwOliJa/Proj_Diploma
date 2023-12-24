982e95973a33689c25ce2e6e5f2b9a315eb61b6b
OlgaKazaka <olgakazaka@yahoo.com>
2023-12-24 16:32:05 +0100

    add sale content for main page

--- src/components/mainSale/index.jsx
+++ src/components/mainSale/index.jsx
@@ -1,12 +1,25 @@
 import styles from "./index.module.css";
 import SaleTitle from "./saleTitle/index.jsx";
 import ProductCards from "../productCards";
+import { useSelector, useDispatch } from "react-redux";
+import { getDiscounts } from "../../store/slices/allDiscountsSlice.js";
+import { useEffect } from "react";
 
 function MainSale() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getDiscounts());
+  }, []);
+  const { discountsList, status } = useSelector((state) => state.discounts);
   return (
     <div className={styles.main_container}>
       <SaleTitle />
-      <ProductCards />
+      <div className={styles.categories_container}>
+        {status === "fulfilled" &&
+          discountsList.slice(0, 4).map((item) => {
+            return <ProductCards key={item.id} {...item} />;
+          })}
+      </div>
     </div>
   );
 }
